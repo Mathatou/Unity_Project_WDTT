4dfd9c18104f0497f96730f5567da0d26ab9fbd5
Mathatou <mathias.aubry@edu.esiee.fr>
2025-05-04 19:15:31 +0200

    J'ai oublié le script
    Il lance l'audio ( pas par la fenêtre !)

--- Unity_Project/Assets/Scripts/NPCInteractionController.cs
+++ Unity_Project/Assets/Scripts/NPCInteractionController.cs
@@ -1,17 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class NPCInteractionController : ObjectInteractionController
 {
-  
-    // Update is called once per frame
-    void Update()
-    {
-     
-    }
+
+    [SerializeField] private AudioClip _clip1 ;
+    [SerializeField] private AudioSource _AudioSource;
+
+    // This method will be called when the player interacts with the NPC
     public override void ObjectInteraction()
     {
+        if(_AudioSource.isPlaying)
+        {
+            _AudioSource.Stop();
+        }
+        else
+        {
+            _AudioSource.PlayOneShot(_clip1);
+        }
+        
         Debug.Log("NPC Interaction Triggered");
     }
 }
