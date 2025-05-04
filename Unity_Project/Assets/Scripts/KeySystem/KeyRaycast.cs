e265f727ff32dd67b5898b6a66af49af2ce18f66
Mathatou <mathias.aubry@edu.esiee.fr>
2025-05-04 18:46:09 +0200

    Commentaires

--- Unity_Project/Assets/Scripts/KeySystem/KeyRaycast.cs
+++ Unity_Project/Assets/Scripts/KeySystem/KeyRaycast.cs
@@ -6,63 +6,70 @@ using UnityEngine.UI;
 
 public class KeyRaycast : MonoBehaviour
 {
+    // Distance the raycast will check for objects
     [SerializeField] private int rayLength = 5;
+
+    // Layer mask for interactable objects
     [SerializeField] private LayerMask InteractionLayerMask;
+
+    // Optional: layer name to exclude from raycast (can be null)
     [SerializeField] private string excluseLayerName = null;
 
-    private KeyItemController _raycastedObject;
+    // Reference to the currently raycasted interactable object
+    private ObjectInteractionController _raycastedObject;
+
+    // Key used to trigger the interaction (default: left mouse click)
     [SerializeField] private KeyCode openDoorKey = KeyCode.Mouse0;
-    [SerializeField] private Image crosshair = null;
+
+    // Tracks if the crosshair (or interaction state) is active
     private bool isCrossHairActive;
+
+    // Ensures the same object isn’t repeatedly selected each frame
     private bool doOnce;
 
+    // Tag used to identify interactable objects
     private string interactableTag = "InteractiveObject";
 
     private void Update()
     {
+        // Store hit info from raycast
         RaycastHit hit;
+
+        // Cast ray forward from the object’s transform
         Vector3 fwd = transform.TransformDirection(Vector3.forward);
 
+        // Combine layer mask: includes the interaction mask and excludes a specified layer
         int mask = (1 << LayerMask.NameToLayer(excluseLayerName)) | InteractionLayerMask.value;
 
-        if(Physics.Raycast(transform.position,fwd,out hit, rayLength, mask ) )
+        // Perform the raycast
+        if (Physics.Raycast(transform.position, fwd, out hit, rayLength, mask))
         {
-            if(hit.collider.CompareTag(interactableTag))
+            // Check if the hit object has the correct tag
+            if (hit.collider.CompareTag(interactableTag))
             {
-                if(!doOnce)
+                // Only get component once per object
+                if (!doOnce)
                 {
-                    _raycastedObject = hit.collider.gameObject.GetComponent<KeyItemController>();
-                    CrosshairChange( true );
+                    _raycastedObject = hit.collider.gameObject.GetComponent<ObjectInteractionController>();
                 }
+
                 isCrossHairActive = true;
                 doOnce = true;
 
-                if(Input.GetKeyDown(openDoorKey))
+                // If the interaction key is pressed, perform the interaction
+                if (Input.GetKeyDown(openDoorKey))
                 {
                     _raycastedObject.ObjectInteraction();
                 }
             }
-                
         }
         else
         {
-            if(isCrossHairActive)
+            // Reset flags if raycast does not hit a valid object
+            if (isCrossHairActive)
             {
-                CrosshairChange(false );
                 doOnce = false;
             }
-        }
-    }
-    private void CrosshairChange( bool on )
-    {
-        if(on && doOnce)
-        {
-            crosshair.color = Color.red;
-        }
-        else
-        {
-            crosshair.color = Color.white;
-            isCrossHairActive = false;
         }
     }
 }
