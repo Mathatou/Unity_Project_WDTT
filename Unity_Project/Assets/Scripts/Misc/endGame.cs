9f7cf8b75e80ca1fdc920cbdb964e7b955ee5174
Mathatou <mathias.aubry@edu.esiee.fr>
2025-05-05 23:14:38 +0200

    Réarrangage
    J'ai bougé tous les scripts qui touche au jeu sans vraiment interagir avec les gameobjects

---
+++ Unity_Project/Assets/Scripts/Misc/endGame.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class endGame : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //SceneManager.LoadScene("EndCinematic");
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+            // Add your game over logic here, such as loading a game over scene or displaying a game over UI.
+            // For example:
+            // SceneManager.LoadScene("GameOverScene");
+        }
+    }
+}
