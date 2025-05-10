8fb28951452f6ca48e115edcacb46d592ad0798b
Mathatou <mathias.aubry2004@gmail.com>
2025-05-10 13:50:19 +0200

    Ajout des dialogues
    Faut juste écrire un texte sympa et faire faire la voix
    Faut voir avec Ach pour qu'elle fasse le NPC

--- Unity_Project/Assets/Scripts/Interactions/NPCInteractionController.cs
+++ Unity_Project/Assets/Scripts/Interactions/NPCInteractionController.cs
@@ -2,23 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class NPCInteractionController : ObjectInteractionController
 {
 
     [SerializeField] private AudioClip _clip1 ;
     [SerializeField] private AudioSource _AudioSource;
-
+    [SerializeField] private PlayableDirector _playableDirector;
+    [SerializeField] private GameObject dialogText;
     // This method will be called when the player interacts with the NPC
     // It will play a sound and log a message to the console
     public override void ObjectInteraction()
     {
-        if(_AudioSource.isPlaying)
+        
+        if (_AudioSource.isPlaying)
         {
             _AudioSource.Stop();
+            _playableDirector.Stop();
+            dialogText.SetActive(false);
         }
         else
         {
+            _playableDirector.Play();
             _AudioSource.PlayOneShot(_clip1);
         }
         
