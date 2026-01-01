50ddb9622abfa9f25c1d333fb5f011066eddaf2b
Mathatou <mathias.aubry@edu.esiee.fr>
2026-01-01 17:20:55 +0100

    Add harmless ghost SFX and audio playback to ghost
    Introduced a new harmless ghost sound effect and updated the GhostInteraction script to play different audio clips based on whether the ghost is a jumpscare or harmless. Modified the gost2 prefab to include audio source references and new serialized fields for sound clips.

--- Unity_Project/Assets/Scripts/Interactions/GhostInteraction.cs
+++ Unity_Project/Assets/Scripts/Interactions/GhostInteraction.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 public class GhostInteraction : ObjectInteractionController
 {
     public bool isJumpscare = false;
+    [SerializeField] private AudioClip harmlessSound;
+    [SerializeField] private AudioClip jumpScareSound;
+    private void Start()
+    {
+        harmlessSound.LoadAudioData();
+        jumpScareSound.LoadAudioData();
+    }
     public override void ObjectInteraction()
     {
+        AudioClip clipToPlay = isJumpscare ? jumpScareSound : harmlessSound;
         if (isJumpscare)
         {
             Debug.Log("Jumpscare !");
@@ -15,6 +23,10 @@ public class GhostInteraction : ObjectInteractionController
         else
         {
             Debug.Log("Ghost is harmless.");
+        }
+        if(clipToPlay != null)
+        {
+            AudioSource.PlayClipAtPoint(clipToPlay, transform.position);
         }
         Destroy(this.gameObject);
     }
